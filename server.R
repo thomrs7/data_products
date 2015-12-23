f0ea3dc0b20c5a8a68eded3b41f4aa77f1967a0a
thom <thom.rs7@gmail.com>
2015-12-23 15:10:01 -0500

    Add Rolling Average

--- server.R
+++ server.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(lubridate)
 library(ggplot2)
 library(viridis)
+library(forecast)
 
 shinyServer(function(input, output) {
     ## Get and prepare data
@@ -11,8 +12,8 @@ shinyServer(function(input, output) {
     wx_data <- mutate(wx_data, 
                       time = ymd_hms(time),
                       areaDescription = as.character(areaDescription)
-        )
-
+    )
+    
     ## Dynamic Dropdowns
     output$choose_vars <- renderUI({
         vars <- c("Temperature" = "Temperature",
@@ -20,7 +21,7 @@ shinyServer(function(input, output) {
                   "Pressure" = "Pressure",
                   'Visibility'= 'Visibility',
                   'Wind_Speed'='Wind_Speed'
-                  )
+        )
         
         selectInput("variable", "Variable:",  vars, 
                     selected = 'Temperature' )
@@ -30,27 +31,32 @@ shinyServer(function(input, output) {
         loc <- as.list(unique(wx_data$areaDescription))
         selectInput("location", "Location:",  loc, 
                     selected = 'Saint Petersburg FL'
-            )
+        )
     })
     
-  ## Create the Plot Based on Inputs
-  output$wxPlot <- renderPlot({
-      if(is.null(input$variable))
-          return()
-     
+    ## Create the Plot Based on Inputs
+    output$wxPlot <- renderPlot({
+        if(is.null(input$variable))
+            return()
+        
         var <- input$variable
         loc <- input$location
-        cnt <- input$count
-    
+        days <- input$days
+        
         data <- filter(wx_data, areaDescription == loc)
-    
+        
+        time_series <- ts(data[var])
+        data$rollingAvg <- ma(time_series, order=days*24)
+        
         p <- ggplot(data,  aes_string(x='time',y=var)) 
-        p <- p + geom_jitter(alpha=.5, size=3, aes_string(color=var)) 
+        p <- p + geom_point(alpha=.5, size=3, aes_string(color=var)) 
         p <- p + scale_color_gradientn(colours = viridis(24),  name=var) 
         p <- p + labs(title = loc, x="Date", y="Degrees Fahrenheit") 
+        p <- p + geom_line( aes(x=time, y=rollingAvg))
+        
         
         return(p)
+        
+    })
     
-  })
-  
 })
